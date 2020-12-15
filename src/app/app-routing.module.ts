148bd2812f7d9d3905742bd3143a172ef997d849
Your Name <you@example.com>
2020-12-15 17:06:42 -0300

    configuração toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path: 'css', component: ToolbarComponent},
+{path: 'css/:option', component: ToolbarComponent},
+{path: '', redirectTo: 'css', pathMatch: 'full'},
+{path: 'css/**', component: PageNotFoundComponent},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
