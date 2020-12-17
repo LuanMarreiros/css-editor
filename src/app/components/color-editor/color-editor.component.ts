c1e6085308b5d19b33571aacad1afd27de547941
Your Name <you@example.com>
2020-12-17 13:04:48 -0300

    adição do conversor hex

--- src/app/components/color-editor/color-editor.component.ts
+++ src/app/components/color-editor/color-editor.component.ts
@@ -7,16 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorEditorComponent implements OnInit {
 
-  color = '';
+  colorRGB = 'rgb(223,40,34)';
+  colorHEX = '';
 
   constructor() { }
 
   ngOnInit(): void {
     this.setPageTitle();
+    setInterval(()=>{
+      this.colorHEX = this.rgbToHEXColor();
+    })
   }
 
   setPageTitle(){
     document.title = 'CSS Editor | Color';
+  }
+
+  rgbToHEXConversion(numberRGB){
+    return Number(numberRGB).toString(16);
+  }
+
+  rgbToHEXColor(){
+    let r = this.colorRGB.replace(/[A-z]|[\\(\\)]/g,'').split(',')[0];
+    let g = this.colorRGB.replace(/[A-z]|[\\(\\)]/g,'').split(',')[1];
+    let b = this.colorRGB.replace(/[A-z]|[\\(\\)]/g,'').split(',')[2];
+
+    return `#${this.rgbToHEXConversion(r)}${this.rgbToHEXConversion(g)}${this.rgbToHEXConversion(b)}`
   }
 
 }
